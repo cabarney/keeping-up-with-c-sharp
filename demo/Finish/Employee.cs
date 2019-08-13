c0d00f2df2188b8b1aba160783dc0a53b0e641a5
Adam Barney <adambarney@quickenloans.com>
2019-08-13 13:16:58 -0500

    cleaning up

--- demo/Finish/Employee.cs
+++ demo/Finish/Employee.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace Demo.Finish
 {
@@ -14,51 +13,26 @@ namespace Demo.Finish
         public TimeSpan TimeWithCompany => DateTime.Now - HireDate;
         public int? ManagerId { get; set; }
         public Manager? Manager { get; set; }
-
+        
         public override string ToString() => $"{FullName} ( Hired {HireDate:MM/dd/yyyy}, Manager: {Manager?.FullName ?? "None"} )";
     }
 
     public class Manager : Employee
     {
-        public Manager(int id, string name, string hireDate) : base(id, name, hireDate) { }
-        
+        public Manager(int id, string name, string hireDate) : base(id, name, hireDate) { }        
         public List<Employee> DirectReports { get; set; } = new List<Employee>();
-        public IEnumerable<TechnicalSkill> TeamSkills() =>
-            DirectReports?.Where(x=>x.GetType() == typeof(SoftwareEngineer))
-                .Select(x => ((SoftwareEngineer)x).Skill)
-                .Distinct()
-                .ToList() ?? throw new InvalidOperationException();
     }
 
     public class SoftwareEngineer : Employee
     {
         public SoftwareEngineer(int id, string name, string hireDate) : base(id, name, hireDate) { }
-
         public TechnicalSkill Skill { get; set; }
     }
 
     public class BusinessAnalyst : Employee
     {
         public BusinessAnalyst(int id, string name, string hireDate) : base(id, name, hireDate) { }
-
         public BusinessArea Area { get; set; }
-    }
-
-    public enum BusinessArea
-    {
-        SalesSystem,
-        OrderFulfillment,
-        Security,
-        ThermonuclearWarfare
-    }
-
-    public enum TechnicalSkill
-    {
-        CSharp,
-        JavaScript,
-        VisualBasic,
-        Whitespace,
-        SQL
     }
     #nullable restore
 }
--- demo/Start/Employee.cs
+++ demo/Start/Employee.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace Demo.Start
 {
@@ -16,9 +15,7 @@ namespace Demo.Start
         }
 
         public DateTime HireDate { get; set; }
-
         public TimeSpan TimeWithCompany => DateTime.Now - HireDate;
-
         public int? ManagerId { get; set; }
         public Manager Manager { get; set; }
 
@@ -31,48 +28,18 @@ namespace Demo.Start
     public class Manager : Employee
     {
         public Manager(int id, string name, string hireDate) : base(id, name, hireDate) { }
-
         public List<Employee> DirectReports { get; set; }
-
-        public IEnumerable<TechnicalSkill> TeamSkills() 
-        {
-            if(DirectReports == null) throw new InvalidOperationException();
-            
-            return DirectReports?.Where(x=>x.GetType() == typeof(SoftwareEngineer))
-                .Select(x => ((SoftwareEngineer)x).Skill)
-                .Distinct()
-                .ToList();
-        }
     }
 
     public class SoftwareEngineer : Employee
     {
         public SoftwareEngineer(int id, string name, string hireDate) : base(id, name, hireDate) { }
-
         public TechnicalSkill Skill { get; set; }
     }
 
     public class BusinessAnalyst : Employee
     {
         public BusinessAnalyst(int id, string name, string hireDate) : base(id, name, hireDate) { }
-
         public BusinessArea Area { get; set; }
-    }
-
-    public enum BusinessArea
-    {
-        SalesSystem,
-        OrderFulfillment,
-        Security,
-        ThermonuclearWarfare
-    }
-
-    public enum TechnicalSkill
-    {
-        CSharp,
-        JavaScript,
-        VisualBasic,
-        Whitespace,
-        SQL
     }
 }
