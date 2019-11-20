ff3f6b7a579b66fee988175fcaf529c722630eaa
Adam Barney <adam@adambarney.com>
2019-11-20 15:22:10 -0600

    More concise NameExtensions

--- demo/Finish/NameExtensions.cs
+++ demo/Finish/NameExtensions.cs
@@ -6,9 +6,7 @@ namespace Demo.Finish
     {
         public static (string firstName, string lastName) ParseName(this string name)
         {
-            if (name == null) throw new ArgumentNullException(nameof(name));
-
-            var nameParts = name.Split(' ');
+            var nameParts = name?.Split(' ') ?? throw new ArgumentNullException(nameof(name));
             return (nameParts[0], nameParts[1]);
         }
     }
